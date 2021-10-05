f45d93e84bb92e501ff35b4f37125da274e56ed9
jorsen <jorsenmejia@gmail.com>
2021-10-05 16:54:06 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+<script>
+	jQuery(function($) {
+		$(document).ready(function() {
+			$('.review-slider > .elementor-container').slick({
+				dots: false,
+				arrows: false,
+				infinite: true,
+				speed: 300,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 1
+						}
+					},
+					{
+						breakpoint: 650,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+		});
+	});
+</script>
